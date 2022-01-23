911f77eac1cb0fd34bb2ae95d7ff3eeec5c3a8e2
Eimantas Dumšė <17217341+eimdum@users.noreply.github.com>
2022-01-23 22:09:36 +0200

    Added missing goal, minor improvements. (#7)

--- readme.md
+++ readme.md
@@ -10,7 +10,16 @@ To use for `dev`, create environment file with name `.env.local` and set keys by
 
 Make sure you have installed `yarn` to your machine before running app.
 
-- Windows - <https://classic.yarnpkg.com/lang/en/docs/install/#windows-stable>
-- MacOS using `brew` - <https://formulae.brew.sh/formula/yarn>
+-   Windows - <https://classic.yarnpkg.com/lang/en/docs/install/#windows-stable>
+-   MacOS using `brew` - <https://formulae.brew.sh/formula/yarn>
 
 To run project in `dev` mode run `yarn start`.
+
+To run project in `prod` mode run `yarn start:prod` (it will run build and serve ). You can also separate commands: `yarn build` to create prod build files, `yarn serve:prod` to serve builded files.
+
+## Technical
+
+-   ReactJs;
+-   React Context for global state management;
+-   Chakra UI as component library;
+-   Local Storage to keep locked gifs;
--- src/containers/GifsListContainer.tsx
+++ src/containers/GifsListContainer.tsx
@@ -1,54 +1,36 @@
 import React from "react";
-import { useQuery } from "react-query";
-import { Flex } from "@chakra-ui/react";
-import { GifsResult } from "@giphy/js-fetch-api";
+import { NotAllowedIcon } from "@chakra-ui/icons";
+import { Flex, Spinner } from "@chakra-ui/react";
 
-import { Gif } from "@store";
 import { GifList } from "@components";
-import { useAppStore, useSpacebarSearch } from "@hooks";
-import { apiRoutes, axiosClient, RequestKeys } from "@api";
+import { useSearchGif, useSpacebarSearch } from "@hooks";
 
 export const GifsListContainer: React.FC = () => {
     useSpacebarSearch();
-    const {
-        gifs,
-        searchGifParams: { q, offset },
-        updateGifs,
-    } = useAppStore();
+    const { gifs, isLoading, isError } = useSearchGif();
 
-    const { data, isLoading, isError, refetch } = useQuery(RequestKeys.SearchGif, async () => {
-        const result = await axiosClient.get<GifsResult>(apiRoutes.searchGif, {
-            params: {
-                q,
-                offset,
-            },
-        });
+    if (isLoading || isError) {
+        return (
+            <Flex flex={1} justifyContent="center" alignItems="center" flexDir="column">
+                {isLoading && (
+                    <>
+                        <Spinner size="lg" mb={4} color="custom.lightGray" />
+                        <Flex color="custom.lightGray" fontWeight="semibold" letterSpacing={1} fontSize={18}>
+                            Loading...
+                        </Flex>
+                    </>
+                )}
 
-        return result.data;
-    });
-
-    React.useEffect(() => {
-        refetch();
-    }, [q, offset, refetch]);
-
-    React.useEffect(() => {
-        const gifData = data?.data.map<Gif>((item) => ({
-            importDateTime: item.import_datetime,
-            url: item.images.downsized.url,
-        }));
-
-        if (gifData) {
-            updateGifs(gifData);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [data]);
-
-    if (isLoading) {
-        return <Flex>Is Loading</Flex>;
-    }
-
-    if (isError) {
-        return <Flex>Failed to load</Flex>;
+                {isError && (
+                    <>
+                        <NotAllowedIcon mb={4} color="red.500" w={16} h={16} />
+                        <Flex color="red.500" fontSize={18}>
+                            Failed to fetch
+                        </Flex>
+                    </>
+                )}
+            </Flex>
+        );
     }
 
     return <GifList gifsList={gifs} />;
